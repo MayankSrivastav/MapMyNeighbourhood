65736f575f9408a3b5c506467fe6126836f69e5b
MayankSrivastav <mayanksri18@yahoo.in>
2016-12-21 19:39:13 +0530

    Implemented search functionality

--- js/app.js
+++ js/app.js
@@ -9,12 +9,68 @@ function initMap() {
     });
 
     var geocoder = new google.maps.Geocoder();
-    getAddressCoordinatesAndSetMarker(geocoder, map);    
+    var markers = ['Bengaluru', 'Tumakuru', 'Hosur', 'Mysuru'];
+    getAddressCoordinatesAndSetMarker(geocoder, map, markers);    
+
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    map.addListener('bounds_changed', function() { 
+        searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function(marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
 }
 
-var markers = ['Bengaluru', 'Tumakuru', 'Hosur', 'Mysuru'];
-
-function getAddressCoordinatesAndSetMarker(geocoder, map) {
+function getAddressCoordinatesAndSetMarker(geocoder, map, markers) {
     for (var i = 0; i < markers.length; ++i) {
         geocoder.geocode({'address' : markers[i]}, function(results, status) {
             if (status === 'OK') {
@@ -28,10 +84,10 @@ function getAddressCoordinatesAndSetMarker(geocoder, map) {
         });
     }    
 
-    displayListOfLocations();
+    displayListOfLocations(markers);
 }
 
-function displayListOfLocations() {
+function displayListOfLocations(markers) {
     markers.forEach(function(item, index){
         var loc = document.createElement("LI");
         var text = document.createTextNode(item);
