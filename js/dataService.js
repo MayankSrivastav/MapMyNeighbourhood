754775b5b661d889357c54814121ab25ebbc5c05
MayankSrivastav <mayanksri18@yahoo.in>
2017-02-18 23:57:31 +0530

    Data service object for locations

---
+++ js/dataService.js
@@ -0,0 +1,50 @@
+// This file provides the JSON object for the 
+// locations with their lat & long
+
+var initialLocations = [
+    {
+        name: "Bangalore",
+        lat: 12.9716,
+        long: 77.5946
+    },
+    {
+        name: "Hosur",
+        lat: 12.7409,
+        long: 77.8253
+    },
+    {
+        name: "Mysuru",
+        lat: 12.2958,
+        long: 76.6394
+    },
+    {
+        name: "Ooty",
+        lat: 11.4064,
+        long: 76.6932
+    },
+    {
+        name: "Coonoor",
+        lat: 11.3530,
+        long: 76.7959
+    },
+    {
+        name: "Puducherry",
+        lat: 11.9139,
+        long: 79.8145 
+    },
+    {
+        name: "Srirangapatna",
+        lat: 12.4216,
+        long: 76.6931
+    },
+    {
+        name: "Tumkur",
+        lat: 13.3392,
+        long: 77.1140
+    },
+    {
+        name: "Tirupati",
+        lat: 13.6288,
+        long: 79.4192
+    }
+];
