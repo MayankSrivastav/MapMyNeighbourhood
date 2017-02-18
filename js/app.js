384c459a7ac9468a8a1364a8e9093a1501a4b378
MayankSrivastav <mayanksri18@yahoo.in>
2017-02-18 23:57:50 +0530

    Added initial functionality for maps

--- js/app.js
+++ js/app.js
@@ -1,53 +1,67 @@
 // This is the main js file for the app
 // It contains the view, model & viewModel
 // for the app
-function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'),{
-        zoom: 8,        
+
+var map;
+var marker;
+
+function DisplayLocation(loc) {
+    var self = this;
+    self.name = loc.name;
+    self.lat = loc.lat;
+    self.long = loc.long;
+    
+    self.visible = ko.observable(true);
+
+    self.infoWindow = new google.maps.InfoWindow({
+        content: self.name
+    });    
+
+    self.marker = new google.maps.Marker({
+        map: map,
+        animation: google.maps.Animation.DROP,
+        position: new google.maps.LatLng(self.lat, self.long)
+    });
+
+    self.marker.addListener('click', function(){
+        if (self.marker.getAnimation() !== null) {
+            self.marker.setAnimation(null);
+        } else {
+            self.marker.setAnimation(google.maps.Animation.BOUNCE);
+            setTimeout(function() {
+                self.marker.setAnimation(null);
+            }, 2000);
+        }
+    });
+
+    google.maps.event.addListener(self.marker, 'click', function(){
+        self.infoWindow.open(map, self.marker);
+    });    
+}
+
+function MapViewModel() {
+    var self = this;
+
+    self.locations = ko.observableArray([]);
+
+    map = new google.maps.Map(document.getElementById('map'),{
+        zoom: 8,
         center: { lat: 12.9716, lng: 77.5946}
     });
 
-    var geocoder = new google.maps.Geocoder();
-    var markers = ['Bengaluru', 'Tumakuru', 'Hosur', 'Mysuru', 'Ooty', 'Coonoor', 'Puducherry', 'Srirangapatna'];
-    var marker;
-    getAddressCoordinatesAndSetMarker(geocoder, map, markers);       
-}
-
-function getAddressCoordinatesAndSetMarker(geocoder, map, markers) {
-    for (var i = 0; i < markers.length; ++i) {
-        geocoder.geocode({'address' : markers[i]}, function(results, status) {
-            if (status === 'OK') {
-                marker = new google.maps.Marker({
-                    map: map,
-                    animation: google.maps.Animation.DROP,
-                    position: results[0].geometry.location
-                });
-                marker.addListener('click', function(){
-                    toggleBounce(this);
-                });
-            } else {
-                alert('Can\'t put marker for the following reason: ' + status);
-            }
-        });
-    }    
-
-    displayListOfLocations(markers);
-}
-
-function toggleBounce(marker) {
-    if (marker.getAnimation() !== null) {
-        marker.setAnimation(null);
-    } else {
-        marker.setAnimation(google.maps.Animation.BOUNCE);
+    for (var loc = 0; loc < initialLocations.length; ++loc) {
+        self.locations.push(new DisplayLocation(initialLocations[loc]));
     }
+
+    // for (var i = 0; i < self.locations().length; ++i) {
+    //     marker = new google.maps.Marker({
+    //         map: map,
+    //         animation: google.maps.Animation.DROP,
+    //         position: new google.maps.LatLng(self.locations()[i].lat, self.locations()[i].long)
+    //     });
+    // }
 }
 
-function displayListOfLocations(markers) {
-    markers.forEach(function(item, index){
-        var loc = document.createElement("LI");
-        var text = document.createTextNode(item);
-        loc.appendChild(text);
-        document.getElementById('locationsUL').appendChild(text);
-        document.getElementById('locationsUL').appendChild(document.createElement("br"));
-    });    
+function initMap() {
+    ko.applyBindings(new MapViewModel());
 }
